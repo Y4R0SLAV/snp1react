67121e8bb969beacc1381f5c2b579c7495761833
Y4ROSLAV <jeros1337@gmail.com>
2023-02-21 20:31:08 +0300

    Fix header: changing the background when scrolling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,43 @@
 import './header.css';
 import navLogo from "../../assets/icons/logo.svg";
+import { useState } from 'react'
 
 export const Header = () => {
-  const buttons = ["Туры", "Создать тур", "Отзывы", "Истории"]
+  const [headerMoving, setHeaderMoving] = useState(false)
 
-  return <header className="header">
+  let headerCN = "header"
+  let logoCN = "header__logo"
+
+  window.addEventListener('scroll', (e) => {
+    if (window.scrollY > 450) {
+      setHeaderMoving(true)
+    } else {
+      setHeaderMoving(false)
+    }
+  })
+
+  const buttons = [
+    { id: 1, title: "Туры", href: "#" },
+    { id: 2, title: "Создать тур", href: "#" },
+    { id: 3, title: "Отзывы", href: "#" },
+    { id: 4, title: "Истории", href: "#" }]
+
+  return <header className={headerMoving ? headerCN + " header__wrapper-fixed" : headerCN}>
     <div className="header__wrapper">
       <div className="header__container _container">
         <nav className="header__nav nav-header">
 
-          <div className="nav__logo">
+          <div className={headerMoving ? logoCN + "nav__logo-black" : logoCN}>
             <img className="nav__logo-img" src={navLogo} alt="" />
           </div>
 
           <div className="nav__btns">
-            {buttons.map(item => <div key={item} className="nav__btn _p-normal"> {item} </div>)}
+            {buttons.map(item => <a key={item.id} href={item.href} className="nav__btn _p-normal" target="_blank"> {item.title} </a>)}
           </div>
 
-          <div className="nav__number _p-normal">
+          <a className="nav__number _p-normal" href="tel:+79999999999">
             +7 999 999 99 99
-          </div>
+          </a>
         </nav>
       </div>
     </div>
