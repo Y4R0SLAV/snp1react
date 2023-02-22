1d89c88b50b2068dec61719d98d29ca6998e8675
Y4ROSLAV <jeros1337@gmail.com>
2023-02-23 00:19:40 +0300

    Refactor texts: now it has its own styles

--- src/components/commons/texts.jsx
+++ src/components/commons/texts.jsx
@@ -1,49 +1,50 @@
 import PropTypes from 'prop-types'
+import s from "./Texts.module.css"
 
 export const BlockTitle = ({ title = "", cn = "", children }) => {
-  return <h2 className={"_title  _h2 " + cn}>
+  return <h2 className={s._title + " " + s._h2 + " " + cn}>
     {title || children}
   </h2>
 }
 
 export const BlockSubtitle = ({ title = "", cn = "", children }) => {
-  return <div className={"stories__subtitle _p-normal  _subtitle " + cn}>
+  return <div className={s._pNormal + " " + s._subtitle + " " + cn}>
     {title || children}
   </div>
 }
 
 export const BlockH1 = ({ title = "", cn = "", children }) => {
-  return <h1 className={"_h1 " + cn}>
+  return <h1 className={s._h1 + " " + cn}>
     {title || children}
   </h1>
 }
 
 export const BlockH2 = ({ title = "", cn = "", children }) => {
-  return <h2 className={"_h2 " + cn}>
+  return <h2 className={s._h2 + " " + cn}>
     {title || children}
   </h2>
 }
 
 export const BlockH3 = ({ title = "", cn = "", children }) => {
-  return <h3 className={"_h3 " + cn}>
+  return <h3 className={s._h3 + " " + cn}>
     {title || children}
   </h3>
 }
 
 export const BlockPSmall = ({ title = "", cn = "", children }) => {
-  return <h3 className={"_p-small " + cn}>
+  return <h3 className={s._pSmall + " " + cn}>
     {title || children}
   </h3>
 }
 
 export const BlockPNormal = ({ title = "", cn = "", children }) => {
-  return <div className={"_p-normal " + cn}>
+  return <div className={s._pNormal + " " + cn}>
     {title || children}
   </div>
 }
 
 export const BlockPBig = ({ title = "", cn = "", children }) => {
-  return <div className={"_p-big " + cn}>
+  return <div className={s._pBig + " " + cn}>
     {title || children}
   </div>
 }
