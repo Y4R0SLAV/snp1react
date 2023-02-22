1230e4718791046c058987d26f88a6258d0d4aa2
Y4ROSLAV <jeros1337@gmail.com>
2023-02-23 00:14:33 +0300

    Refactor ChooseItem: combined with StoriesItem

--- src/components/commons/ChooseStoriesItem.jsx
+++ src/components/commons/ChooseStoriesItem.jsx
@@ -1,35 +1,38 @@
 import { BlockH3, BlockPSmall, BlockPBig, BlockPNormal } from './texts'
 import { PropTypes } from 'prop-types'
-import "./chooseStoriesItem.css"
+import s from "./ChooseStoriesItem.module.css"
 
 export const chooseType = "choose"
 export const storiesType = "stories"
 
 // type = chooseType || storiesType
 export const ChooseStoriesItem = ({ id, title, children, links = [], type }) => {
-  let cn = "choose__item item" + id
+  let a = "item" + id
+  let b = "story" + id
+
+  let cn = s.chooseItem + " " + s[a]
 
   if (type === storiesType) {
-    cn = "stories__item story" + id
+    cn = s.storiesItem + " " +s[b]
   }
 
   return <div className={cn}>
-    <div className="item__info">
-      <BlockH3 cn="item__title" title={title} />
-      <BlockPSmall cn="item__price">
+    <div className={s.itemInfo}>
+      <BlockH3 cn={s.itemTitle} title={title} />
+      <BlockPNormal cn={s.itemPrice}>
         {children}
-      </BlockPSmall>
+      </BlockPNormal>
     </div>
 
     {
       type === storiesType 
-      ? <div className="item__detNlinks">
-          <BlockPBig cn="item__details" title="Подробнее" />
-          <div className="item__links">
-            {links.map(link => <BlockPNormal key={link.id}><a className="item__link" href={link.url}>{link.title}</a></BlockPNormal>)}
+      ? <div className={s.itemDetNlinks}>
+          <BlockPBig cn={s.itemDetails} title="Подробнее" />
+          <div className={s.itemLinks}>
+            {links.map(link => <BlockPNormal key={link.id}><a className={s.itemLink} href={link.url}>{link.title}</a></BlockPNormal>)}
           </div>
         </div>
-        : <BlockPBig cn="item__details" title="Подробнее" />
+        : <BlockPBig cn={s.itemDetails} title="Подробнее" />
     }
   </div>
 }
