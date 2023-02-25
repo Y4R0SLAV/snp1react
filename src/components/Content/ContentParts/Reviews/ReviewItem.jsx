1f6abd447103b111083d718cf331fd6d5d3fd79a
Y4ROSLAV <jeros1337@gmail.com>
2023-02-25 21:38:16 +0300

    Refactor review: moved the reviewItem to a separate component

---
+++ src/components/Content/ContentParts/Reviews/ReviewItem.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import { BlockPBig, BlockH3, BlockPSmall } from '../../../commons/texts'
+import s from "./ReviewItem.module.css"
+
+export const ReviewItem = ({ children, name, tourName, imgSrc }) => {
+  return <div className={s.reviews__item }>
+    <BlockPBig cn={s.review__text}> {children} </BlockPBig>
+
+    <div className={s.review__person}>
+      <div className={s.review__info}>
+        <BlockH3 cn={s.review__name} title={name} />
+        <BlockPSmall cn={s.review__to}>Тур: {tourName}</BlockPSmall>
+      </div>
+      <div className={s.review__photo}>
+        <img src={imgSrc} alt="" />
+      </div>
+    </div>
+  </div>
+}
+
+ReviewItem.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  name: PropTypes.string.isRequired,
+  tourName: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+}
